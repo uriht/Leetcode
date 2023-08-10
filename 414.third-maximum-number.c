a0ee842c0936a14f916f600cf3cbf7f153b94228
THIRUKKURAL SELVAN <akauriht@gmail.com>
2023-08-10 20:46:00 +0530

    Third Maximum Number

---
+++ 414.third-maximum-number.c
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=414 lang=c
+ *
+ * [414] Third Maximum Number
+ */
+
+// @lc code=start
+int thirdMax(int* arr, int n) {
+    long long fmax = LLONG_MIN; 
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > fmax) {
+            fmax = arr[i];
+        }
+    }
+
+    long long smax = LLONG_MIN;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > smax && arr[i] < fmax) {
+            smax = arr[i];
+        }
+    }
+
+    long long tmax = LLONG_MIN;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > tmax && arr[i] < smax) {
+            tmax = arr[i];
+        }
+    }
+
+    if (tmax == LLONG_MIN) {
+        return fmax;
+    }
+    return tmax;
+}
+
+// @lc code=end
+
